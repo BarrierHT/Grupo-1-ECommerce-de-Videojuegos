7294e1247c5dc7c10971734f11822ff63bad2eaf
JaviCaiola <javiteen1250@gmail.com>
2023-07-31 02:28:23 -0300

    configuracion del archivo app.js para la lectura de la carpeta public

--- DigitalGaming/src/app.js
+++ DigitalGaming/src/app.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname+'/'));
+app.use(express.static(path.join(__dirname, 'public/css')));
 app.set(process.env.PORT);
 
 app.get('/', (req, res, next) => {
