e22badbeefe3c3a5077f9da7ff19cbd90f12ec58
JaviCaiola <javiteen1250@gmail.com>
2023-07-31 05:14:41 -0300

    implementacion del menu lateral

--- DigitalGaming/src/public/js/main.js
+++ DigitalGaming/src/public/js/main.js
@@ -1,1 +1,10 @@
-console.log('scrpts en funcionamiento')
+const boton = document.querySelector(".btn-menu");
+const slidemenu = document.querySelector(".menu-lat");
+const botoncerrar = document.querySelector(".close")
+
+boton.addEventListener('click', () => {
+    console.log(slidemenu.classList.toggle('activo'));
+})
+botoncerrar.addEventListener('click', ()=>{
+    slidemenu.classList.toggle('activo')
+} )
