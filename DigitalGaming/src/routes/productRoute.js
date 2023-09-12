fb5500fc6de21ea9531af255198c01ad4b061c1a
NahuelALB <ivanalbarado8@gmail.com>
2023-09-11 23:57:15 -0300

    CRUD implementado con el Front End

--- DigitalGaming/src/routes/productRoute.js
+++ DigitalGaming/src/routes/productRoute.js
@@ -6,7 +6,7 @@ const shopController = require('../controllers/shop');
 router.get('/productCart', shopController.getCart);
 router.get('/productDetail-standart', shopController.getDetailCartstandart);
 
-router.get('/editProductForm/:productId', shopController.getEditProduct);
+// router.get('/editProductForm/:productId', shopController.getEditProduct);
 
 router.get('/', shopController.getProducts); //Listado de productos
 
@@ -14,11 +14,11 @@ router.get('/create', shopController.getAddProduct); //Form de agregar productos
 
 router.get('/:productId', shopController.getProductDetail); //Detalle de un producto particular
 
+router.put('/:productId', shopController.putEditProduct); //Editar producto
+
 router.post('/', shopController.postAddProduct); //Crear producto
 
 router.get('/:productId/edit', shopController.getEditProduct); //Formulario de editar producto
-
-router.put('/:productId', shopController.putEditProduct); //Editar producto
 
 router.delete('/:productId', shopController.deleteProduct); //Borrar producto
 
