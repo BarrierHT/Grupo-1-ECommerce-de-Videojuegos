f2b0773c0a4561a2f5cefbb717d74ecd7aa7ffda
JaviCaiola <javiteen1250@gmail.com>
2023-09-13 19:56:14 -0300

    funcionalidad boton del dash izq

--- DigitalGaming/src/public/js/dashboard.js
+++ DigitalGaming/src/public/js/dashboard.js
@@ -6,15 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
         option.addEventListener("click", function () {
             const viewId = this.id;
             showView(viewId);
+
+            options.forEach(item => {
+                item.classList.remove("selected");
+            });
+            this.classList.add("selected");
         });
     });
+
+
 
     function showView(viewId) {
         const views = document.querySelectorAll(".contains-dash > div");
         views.forEach(view => {
             view.style.display = "none";
         });
-
         const selectedView = document.getElementById(`dash-${viewId}`);
         if (selectedView) {
             selectedView.style.display = "block";
