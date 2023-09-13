f0f4f51183a7ea61203da819d618818fab979677
JaviCaiola <javiteen1250@gmail.com>
2023-09-12 22:16:33 -0300

    iniciando estilos deldasjboard

--- DigitalGaming/src/controllers/admin.js
+++ DigitalGaming/src/controllers/admin.js
@@ -2,5 +2,5 @@ const path = require('path');
 const fs = require('fs');
 
 exports.getDash = (req, res, next) => {
-    res.render('admin/dashboard')
+    res.render('admin/dashboard.ejs')
 };
--- DigitalGaming/src/routes/adminRoute.js
+++ DigitalGaming/src/routes/adminRoute.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 router.get('/dashboard', adminController.getDash);
+
+module.exports = router;
