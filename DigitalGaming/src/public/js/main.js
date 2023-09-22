c020df4ba46f5787c70569febe1ca9e31323ddc1
JaviCaiola <javiteen1250@gmail.com>
2023-09-22 13:28:51 -0300

    logica del modal

--- DigitalGaming/src/public/js/main.js
+++ DigitalGaming/src/public/js/main.js
@@ -3,17 +3,24 @@ const slidemenu = document.querySelector(".menu-lat");
 const botoncerrar = document.querySelector(".close")
 
 boton.addEventListener('click', () => {
-    console.log(slidemenu.classList.toggle('activo'));
+    slidemenu.classList.toggle('activo');
 })
 botoncerrar.addEventListener('click', ()=>{
     slidemenu.classList.toggle('activo')
 } )
 document.addEventListener("DOMContentLoaded", function () {
     const home = document.querySelectorAll(".home-navigate");
-    
     home.forEach(div => {
         div.addEventListener("click", function () {
             window.location.href = "/";
         });
+    });
+});
+
+let links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+      const endpoint = link.getAttribute('data-endpoint');
+      window.location.href = endpoint;
     });
 });
