b3493bf68779b82a18de75fc15f86eb5f66a0cce
JaviCaiola <javiteen1250@gmail.com>
2023-09-23 14:37:55 -0300

    animacion del manu lateral creada

--- DigitalGaming/src/public/js/main.js
+++ DigitalGaming/src/public/js/main.js
@@ -1,12 +1,18 @@
 const boton = document.querySelector(".btn-menu");
 const slidemenu = document.querySelector(".menu-lat");
 const botoncerrar = document.querySelector(".close")
+const spanlat2 = document.querySelector(".span-lat-2");
+const spanlat = document.querySelector(".span-lat");
 
 boton.addEventListener('click', () => {
     slidemenu.classList.toggle('activo');
+    spanlat.classList.toggle('animate');
+    spanlat2.classList.toggle('animate');
 })
 botoncerrar.addEventListener('click', ()=>{
     slidemenu.classList.toggle('activo')
+    spanlat.classList.toggle('animate');
+    spanlat2.classList.toggle('animate');
 } )
 document.addEventListener("DOMContentLoaded", function () {
     const home = document.querySelectorAll(".home-navigate");
