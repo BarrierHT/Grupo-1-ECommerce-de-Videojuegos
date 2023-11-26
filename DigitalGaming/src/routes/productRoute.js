c584948c2a8bcad88a2aec214dac6b238a08c022
NahuelAlbarado <rocketcs95@gmail.com>
2023-11-26 20:48:36 -0300

    Casi resuelto las validaciones del lado del backend

--- DigitalGaming/src/routes/productRoute.js
+++ DigitalGaming/src/routes/productRoute.js
@@ -6,6 +6,8 @@ const shopController = require('../controllers/shop');
 const isAuth = require('../middlewares/is-Auth').isAuth;
 const multerProduct = require('../middlewares/multerProduct');
 
+const validationProduct = require('../middlewares/validationProduct');
+
 router.get('/productCart', isAuth, shopController.getCart);
 router.get('/productDetail-standart', shopController.getDetailCartstandart);
 
@@ -18,6 +20,7 @@ router.post(
     { name: 'portada', maxCount: 1 },
     { name: 'video', maxCount: 1 },
   ]),
+  validationProduct,
   shopController.postAddProduct,
 ); //Crear producto
 
@@ -25,7 +28,11 @@ router.get('/', shopController.getProducts); //Listado de productos
 router.get('/:productId', shopController.getProductDetail); //Detalle de un producto particular
 
 router.get('/:productId/edit', /* isAuth */ shopController.getEditProduct); //Formulario de editar producto
-router.put('/:productId/edit', /* isAuth */ shopController.putEditProduct); //Editar producto
+router.put(
+  '/:productId/edit',
+  /* isAuth */ validationProduct,
+  shopController.putEditProduct,
+); //Editar producto
 
 router.delete('/:productId', /* isAuth */ shopController.deleteProduct); //Borrar producto
 
