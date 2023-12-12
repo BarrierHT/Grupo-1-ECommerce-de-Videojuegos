a0ccc7164fe819203fc07ca3c03165b0b19e20bd
Aldana <102806906+AldanaTorrejon@users.noreply.github.com>
2023-12-12 08:28:19 -0300

    Fix error controller user update

--- DigitalGaming/src/controllers/user.js
+++ DigitalGaming/src/controllers/user.js
@@ -32,7 +32,7 @@ exports.deleteUserByEmail = async (req, res, next) => {
 	try {
 		// console.log('bar DELETING', req.body);
 
-		const email = req.body.email; // Obtén el correo electrónico del cuerpo de la solicitud
+		const email = req.body.email; 
 
 		const user = await User.findOne({
 			where: {
@@ -61,7 +61,7 @@ exports.updateUserByEmail = async (req, res, next) => {
 	try {
 		// console.log('foo UPDATING');
 
-		const email = req.body.email; // Obtén el correo electrónico del cuerpo de la solicitud
+		const email = req.body.email; 
 		const { name, lastname } = req.body; // Los nuevos datos que quieres actualizar
 
 		const user = await User.findOne({
@@ -76,7 +76,7 @@ exports.updateUserByEmail = async (req, res, next) => {
 		console.log('UPDATING ', user.dataValues);
 		// console.log('UPDATING ', newData);
 
-		// Actualiza los campos del usuario con los nuevos datos
+	
 		// for (const key in newData) {
 		// 	if (newData.hasOwnProperty(key)) {
 		// 		user[key] = newData[key];
@@ -84,7 +84,7 @@ exports.updateUserByEmail = async (req, res, next) => {
 		// }
 
 		user['name'] = name;
-		user['lastname'] = lastname;
+		user['last_name'] = lastname;
 
 		// Guarda los cambios en la base de datos
 		await user.save();
@@ -96,3 +96,4 @@ exports.updateUserByEmail = async (req, res, next) => {
 		return res.status(400).json({ message: error.message });
 	}
 };
+
